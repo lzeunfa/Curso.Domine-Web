ca80627d984120ecbcb7e3befa3537c6811c3288
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-02-26 20:46:44 -0300

    exercicio

---
+++ JavaScript/teste.js
@@ -0,0 +1,19 @@
+var a = 10
+
+var b = 20
+
+var c = null
+
+document.write(`Essas são as variáveis: a= ${a} b=${b} c=${c} <br>`)
+
+document.write(`Agora vamos realizar trocas <br>`)
+
+var c = b
+
+var b = a
+
+var a = c
+
+var c = null
+
+document.write(`Essas são as variáveis: a= ${a} b=${b} c=${c} <br>`)
