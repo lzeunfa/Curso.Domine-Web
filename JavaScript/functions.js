f116e39d92662a86f3b7c52eab41d10aa6c5a58a
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-03-18 19:03:22 -0300

    estudando

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -37,10 +37,47 @@ console.log(soma(7))//= not a number;
 */
 
 //Funções anônimas e wrapper
-
+/*
 var exibirMsg = function(nome){
     console.log(`Olá ${nome}, está tudo bem?`);
 }
 
 exibirMsg('Jorge');
+*/
 
+
+
+//function de call back
+/*
+function exibirArtigo(id,callBackSucesso,callBackErro){
+    //lógica recuperar id via requisição http
+    if(true){
+        callBackSucesso("Funções de callback em JS","Funções de callback são muito utilizadas");
+    }else{
+        callBackErro("Erro ao recuperar os dados");
+    }
+}
+
+//técnica de wrapper onde uma variavel embrulha uma function
+var callBackSucesso = function(titulo,descricao){
+    console.log(`${titulo} ${descricao}`);
+};
+
+var callBackErro = function(erro){
+    console.log(`${erro}`);
+};
+
+
+exibirArtigo(1, callBackSucesso, callBackErro);
+*/
+
+
+//Funções nativas
+//propriedade tem na w3schools js string reference
+var nome = "Arthur Nóbrega Chacon";
+console.log(nome.length);
+console.log(nome.charAt(6));
+console.log(nome.indexOf("r"));
+console.log(nome.replace("Nóbrega", "José"));
+console.log(nome.substring(5,12));
+console.log(nome.slice(10));
