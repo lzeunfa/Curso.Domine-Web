5f8069cc29b0e547b91ad4c896cab1b8525eae74
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-04-10 19:59:44 -0300

    novo exercicio

---
+++ JavaScript/arrays.js
@@ -0,0 +1,14 @@
+let lista_frutas = [];
+
+lista_frutas[0] = "Banana";
+lista_frutas[1] = "Laranja";
+lista_frutas[2] = "Manga";
+lista_frutas[3] = "Mamão";
+
+let deletar;
+
+console.log(`Essa é a lista de compras:`);
+for (let i = 0; i<=lista_frutas.length-1; i++){
+    console.log(`${lista_frutas[i]}`);
+}
+
