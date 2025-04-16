4760972716698c3eb5ac00e30eb942f9e88bfbc8
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-04-15 21:02:27 -0300

    Create arraysmulti.js

---
+++ JavaScript/arraysmulti.js
@@ -0,0 +1,19 @@
+let lista_coisas = ['objetos', 'roupas'];
+    lista_coisas[0] = [];
+    lista_coisas[1] = [];
+
+    lista_coisas[0][0] = 'Celular';
+    lista_coisas[0][1] = 'Relógio';
+
+    lista_coisas[1][0] = 'calças';
+    lista_coisas[1][1] = 'Blusas';
+
+console.log(lista_coisas);
+
+
+//como objeto
+let obj = {};
+obj["Nome"] = {};
+obj["Nome"][1] = "eliseu";
+console.log(obj)
+
