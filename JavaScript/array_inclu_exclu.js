71445f4cfe2a91e6b8c7e35ac9985ac63ec807f3
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-04-16 20:25:05 -0300

    Create array_inclu_exclu.js

---
+++ JavaScript/array_inclu_exclu.js
@@ -0,0 +1,28 @@
+let lista_coisas = [];
+lista_coisas['frutas'] = [];
+lista_coisas['pessoas'] =[];
+lista_coisas['frutas'].push("Banana");
+lista_coisas['frutas'].push("Uva");
+lista_coisas['frutas'].unshift("Pêssego");
+lista_coisas['pessoas'].push("heloisa");
+
+console.log(lista_coisas);
+
+
+/*
+//inncluir no fim do array
+lista_frutas.push("Uva");
+console.log(lista_frutas);
+
+//incluir no inicio do array
+lista_frutas.unshift("Pêssego");
+console.log(lista_frutas);
+
+//exclui no fim do array
+lista_frutas.pop();
+console.log(lista_frutas);
+
+//exclui no inicio do array
+lista_frutas.shift();
+console.log(lista_frutas);
+*/
