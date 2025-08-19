3d76616e596e52c099f0fceaa366228ec8b530c9
Eliseu <eliseu.nfa@gmail.com>
2025-08-19 20:21:23 -0300

    avançando nos estudos do ecmascript

---
+++ ES6/00_pildar_do_encapsulamento.js
@@ -0,0 +1,23 @@
+class Tv{
+    constructor(){
+        this._relacaoCanais = [2,4,5,7,10];
+        this._canalAtivo = 5;
+        this._volume = 5;
+    }
+
+    get canalAtivo(){
+        return this._canalAtivo;
+    }
+
+    set canalAtivo(canal){
+        //
+        if(this._relacaoCanais.indexOf(canal) !== -1){
+            this._canalAtivo = canal;
+        }
+    }
+}
+
+let tv = new Tv();
+
+tv.canalAtivo = 10;
+console.log(tv.canalAtivo);
