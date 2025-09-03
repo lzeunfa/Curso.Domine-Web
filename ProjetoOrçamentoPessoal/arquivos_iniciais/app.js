97f6a80491de04146a1e41009ddb396a0b0812e1
Eliseu <eliseu.nfa@gmail.com>
2025-09-03 09:12:39 -0300

    porjeto orçamento pessoal finalizado na conclusão do módulo de ES6

--- ProjetoOrçamentoPessoal/arquivos_iniciais/app.js
+++ ProjetoOrçamentoPessoal/arquivos_iniciais/app.js
@@ -61,6 +61,7 @@ class Bd{
                 continue;
             }
 
+            despesa.id = i;
             despesas.push(despesa);
         }
 
@@ -104,6 +105,10 @@ class Bd{
         }
 
         return despesasFiltradas;
+    }
+
+    remover(id){
+        localStorage.removeItem(id);
     }
 }
 
@@ -190,6 +195,21 @@ function carregaListaDespesa(despesas = [], filtro = false){
 
         linha.insertCell(2).innerHTML = d.descricao;
         linha.insertCell(3).innerHTML = d.valor;
+
+        //criar o botao de exclusao
+        let btn = document.createElement("button");
+        btn.className = "btn btn-danger";
+        btn.innerHTML = `<i class="fas fa-times "></i>`
+        btn.id = `id_despesa_${d.id}`;
+        btn.onclick = function (){
+
+            let id = this.id.replace("id_despesa_", "");
+
+            bd.remover(id);
+
+            window.location.reload();
+        };
+        linha.insertCell(4).append(btn);
 
     })
 }
