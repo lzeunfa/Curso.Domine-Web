27fc99e4d6bec2c5577ed5450ff4a6cbae606a28
Eliseu <eliseu.nfa@gmail.com>
2025-09-02 21:04:36 -0300

    Quase finalizando o projeto de conclusão de módulo de ES6 udemy

--- ProjetoOrçamentoPessoal/arquivos_iniciais/app.js
+++ ProjetoOrçamentoPessoal/arquivos_iniciais/app.js
@@ -44,6 +44,67 @@ class Bd{
 
         localStorage.setItem("id",id)
     }
+
+    recuperarTodosRegistros(){
+        //array de despesas
+        let despesas = [];
+
+        let id = localStorage.getItem("id");
+        
+        //recuperar todas as despesas cadastradas em localstorage
+        for(let i =1;i<=id;i++){
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            //existe a possibilidade de haver indices que foram pulados
+            //neste caso nos vamos pular os indices
+            if(despesa===null){
+                continue;
+            }
+
+            despesas.push(despesa);
+        }
+
+        return despesas;
+    }
+
+    pesquisar(despesa){
+        let despesasFiltradas = [];
+
+        despesasFiltradas = this.recuperarTodosRegistros();
+        
+
+        //ano
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano);
+        }
+
+        //mes
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes);
+        }
+
+        //dia
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia);
+        }
+
+        //tipo
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo);
+        }
+
+        //descricao
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao);
+        }
+
+        //valor
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor);
+        }
+
+        return despesasFiltradas;
+    }
 }
 
 let bd = new Bd();
@@ -71,6 +132,13 @@ function cadastrarDespesa(){
         document.getElementById("modal_btn").innerHTML = "Voltar";
         document.getElementById("modal_btn").className = "btn btn-success";
 
+        ano.value = '';
+        mes.value = '';
+        dia.value = '';
+        tipo.value = '';
+        descricao.value = '';
+        valor.value = '';
+
 
         $('#modalRegistroDespesa').modal('show');
     }else{
@@ -82,4 +150,61 @@ function cadastrarDespesa(){
 
         $('#modalRegistroDespesa').modal('show');
     }    
+}
+
+function carregaListaDespesa(despesas = [], filtro = false){
+    if(despesas.length == 0 && filtro == false){
+        despesas = bd.recuperarTodosRegistros();
+    }
+
+    //selecioanndo elemento tbody
+    let listaDespesas = document.getElementById("listaDespesas");
+    listaDespesas.innerHTML = '';
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmicas
+    despesas.forEach(function(d){
+
+        //criando tr linha
+        let linha = listaDespesas.insertRow();
+
+        //criar colunas td
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação';
+            break;
+
+            case '2': d.tipo = 'Educação';
+            break;
+
+            case '3': d.tipo = 'Lazer';
+            break;
+
+            case '4': d.tipo = 'Saúde';
+            break;
+
+            case '5': d.tipo = 'Transporte';
+            break;
+        }
+        linha.insertCell(1).innerHTML = d.tipo;
+
+        linha.insertCell(2).innerHTML = d.descricao;
+        linha.insertCell(3).innerHTML = d.valor;
+
+    })
+}
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor);
+
+    let despesas = bd.pesquisar(despesa);
+
+    this.carregaListaDespesa(despesas,true);
 }
