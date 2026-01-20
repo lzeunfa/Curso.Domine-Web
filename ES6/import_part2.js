c54c2c50f007c388a2354b0c4822df89d15787b5
Eliseu <eliseu.nfa@gmail.com>
2026-01-20 16:23:15 -0300

    Update import_part2.js

--- ES6/import_part2.js
+++ ES6/import_part2.js
@@ -1,11 +1,17 @@
-setTimeout(()=>{
-import('./import/lib.mjs')
-    .then(module => console.log(module.saudacao()));
+let saudar = async function() {
+    const modulo = await import('./import/lib.mjs');
+
+    setTimeout(async ()=>{
+        const msg = modulo.saudacao();
+        console.log(msg);
+
     return;
 }, 500);
+}
+saudar();
 
 setTimeout(()=>{
 import('./import/lib.mjs')
-    .then(module => console.log(module.getEnderecoByCEP(71065072).then(dados => console.log(dados))));
+    .then(module => module.getEnderecoByCEP(71065072).then(dados => console.log(dados.localidade)));
     return;
 }, 2000);
